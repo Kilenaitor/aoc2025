2388bd02f5d09ed205e297a5681f41b4c79553cd
Kyle Minshall <kyleminshall@gmail.com>
2025-12-09 00:55:42 -0800

    [WIP] Day 9 part 1

--- rust/src/day09/day9.rs
+++ rust/src/day09/day9.rs
@@ -17,90 +17,37 @@ pub fn run(part: Part) {
 }
 
 fn part1() {
-    let input = fs::read_to_string("src/day5/input.txt")
+    let points = fs::read_to_string("src/day09/input.txt")
+        .unwrap()
+        .trim()
+        .split("\n")
+        .map(|line| {
+            let (x, y) = line.split_once(',').unwrap();
+            (x.parse::<i64>().unwrap(), y.parse::<i64>().unwrap())
+        })
+        .collect::<Vec<(i64, i64)>>();
+
+    let mut max_area: i64 = 0;
+    for i in 0..points.len() {
+        for j in (i + 1)..points.len() {
+            let area =
+                ((points[i].0 - points[j].0).abs() + 1) * ((points[i].1 - points[j].1).abs() + 1);
+            if area > max_area {
+                max_area = area;
+            }
+        }
+    }
+
+    println!("{}", max_area)
+}
+
+fn part2() {
+    let input = fs::read_to_string("src/day09/input.txt")
         .unwrap()
         .trim()
         .split_once("\n\n")
         .map(|i| (i.0.to_string(), i.1.to_string()))
         .unwrap();
-    let ranges = input
-        .0
-        .split("\n")
-        .map(|range| {
-            range
-                .trim()
-                .split_once("-")
-                .map(|num| (num.0.parse::<u64>().unwrap(), num.1.parse::<u64>().unwrap()))
-        })
-        .map(|range| range.unwrap())
-        .collect::<Vec<(u64, u64)>>();
-    let ingredient_ids = input
-        .1
-        .split("\n")
-        .map(|ingredient| ingredient.trim().parse::<u64>().unwrap())
-        .collect::<Vec<u64>>();
 
-    let mut num_fresh = 0;
-    for ingredient_id in ingredient_ids {
-        for (low, high) in &ranges {
-            if ingredient_id >= *low && ingredient_id <= *high {
-                num_fresh += 1;
-                break;
-            }
-        }
-    }
-    println!("{:?}", num_fresh);
-}
-
-fn part2() {
-    let mut input = fs::read_to_string("src/day5/input.txt")
-        .unwrap()
-        .trim()
-        .split_once("\n\n")
-        .unwrap()
-        .0
-        .split("\n")
-        .map(|range| {
-            range
-                .trim()
-                .split_once("-")
-                .map(|num| (num.0.parse::<u64>().unwrap(), num.1.parse::<u64>().unwrap()))
-        })
-        .map(|range| range.unwrap())
-        .collect::<Vec<(u64, u64)>>();
-
-    // Sorting the ranges by their opening value
-    // so we don't need to scan through the collapsed
-    // list.
-    input.sort_by(|a, b| a.0.cmp(&b.0));
-
-    let mut ranges: Vec<(u64, u64)> = vec![];
-
-    for i in 0..input.len() {
-        if ranges.len() == 0 {
-            ranges.push(input[i]);
-            continue;
-        }
-
-        let candidate_range = input[i];
-        let highest_range_index = ranges.len() - 1;
-        let highest_range = &mut ranges[highest_range_index];
-        if candidate_range.0 >= highest_range.0 && candidate_range.0 <= highest_range.1 {
-            // We have an overlap!
-            highest_range.1 = if candidate_range.1 > highest_range.1 {
-                candidate_range.1
-            } else {
-                highest_range.1
-            };
-        } else {
-            ranges.push(candidate_range);
-        }
-    }
-
-    let mut distinct_ids = 0;
-    for (low, high) in ranges {
-        distinct_ids += high - low + 1;
-    }
-
-    println!("{}", distinct_ids);
+    println!("part 2");
 }
