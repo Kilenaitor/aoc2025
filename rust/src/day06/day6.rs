9b815bb463ff11b70758c555420a29024680ebfe
Kyle Minshall <kyleminshall@gmail.com>
2025-12-09 00:55:36 -0800

    Add Day6 (<3) implementation

--- rust/src/day06/day6.rs
+++ rust/src/day06/day6.rs
@@ -1,5 +1,6 @@
 use crate::Part;
-use std::fs;
+use regex::Regex;
+use std::{fs, iter::Rev, str::Chars};
 
 pub fn run(part: Part) {
     match part {
@@ -17,90 +18,94 @@ pub fn run(part: Part) {
 }
 
 fn part1() {
-    let input = fs::read_to_string("src/day5/input.txt")
+    let re = Regex::new(r"\s{2,}").unwrap();
+    let mut input = fs::read_to_string("src/day06/input.txt")
         .unwrap()
         .trim()
-        .split_once("\n\n")
-        .map(|i| (i.0.to_string(), i.1.to_string()))
-        .unwrap();
-    let ranges = input
-        .0
         .split("\n")
-        .map(|range| {
-            range
-                .trim()
-                .split_once("-")
-                .map(|num| (num.0.parse::<u64>().unwrap(), num.1.parse::<u64>().unwrap()))
-        })
-        .map(|range| range.unwrap())
-        .collect::<Vec<(u64, u64)>>();
-    let ingredient_ids = input
-        .1
-        .split("\n")
-        .map(|ingredient| ingredient.trim().parse::<u64>().unwrap())
-        .collect::<Vec<u64>>();
+        .map(|line| re.replace_all(line, " ").to_string())
+        .collect::<Vec<String>>();
 
-    let mut num_fresh = 0;
-    for ingredient_id in ingredient_ids {
-        for (low, high) in &ranges {
-            if ingredient_id >= *low && ingredient_id <= *high {
-                num_fresh += 1;
-                break;
+    let ops = input
+        .pop()
+        .unwrap()
+        .trim()
+        .split(" ")
+        .map(|op| op.to_string())
+        .collect::<Vec<String>>();
+    let numbers = input
+        .into_iter()
+        .map(|row| {
+            row.trim()
+                .split(" ")
+                .map(|number| number.parse::<u64>().unwrap())
+                .collect::<Vec<u64>>()
+        })
+        .collect::<Vec<Vec<u64>>>();
+
+    let mut totals: Vec<u64> = vec![];
+    for i in 0..ops.len() {
+        match ops[i].as_str() {
+            "*" => {
+                let mut total = 1;
+                for j in 0..numbers.len() {
+                    total *= numbers[j][i];
+                }
+                totals.push(total);
             }
-        }
+            "+" => {
+                let mut total = 0;
+                for j in 0..numbers.len() {
+                    total += numbers[j][i];
+                }
+                totals.push(total);
+            }
+            _ => panic!("Unexpected operation"),
+        };
     }
-    println!("{:?}", num_fresh);
+
+    println!("{:?}", totals.into_iter().reduce(|t, e| t + e).unwrap());
 }
 
 fn part2() {
-    let mut input = fs::read_to_string("src/day5/input.txt")
+    let rows = fs::read_to_string("src/day06/input.txt")
         .unwrap()
-        .trim()
-        .split_once("\n\n")
-        .unwrap()
-        .0
         .split("\n")
-        .map(|range| {
-            range
-                .trim()
-                .split_once("-")
-                .map(|num| (num.0.parse::<u64>().unwrap(), num.1.parse::<u64>().unwrap()))
-        })
-        .map(|range| range.unwrap())
-        .collect::<Vec<(u64, u64)>>();
+        .map(|line| line.to_string())
+        .collect::<Vec<String>>();
 
-    // Sorting the ranges by their opening value
-    // so we don't need to scan through the collapsed
-    // list.
-    input.sort_by(|a, b| a.0.cmp(&b.0));
+    let line_len = rows[0].len();
+    let mut iters = rows
+        .iter()
+        .map(|line| line.chars().rev())
+        .collect::<Vec<Rev<Chars>>>();
 
-    let mut ranges: Vec<(u64, u64)> = vec![];
-
-    for i in 0..input.len() {
-        if ranges.len() == 0 {
-            ranges.push(input[i]);
-            continue;
+    let mut totals: Vec<u64> = vec![];
+    let mut numbers: Vec<u64> = vec![];
+    for _ in 0..line_len {
+        let mut number = 0u64;
+        for j in 0..(iters.len() - 1) {
+            let val = iters[j].next().unwrap();
+            if val != ' ' {
+                number *= 10;
+                number += val.to_digit(10).unwrap() as u64;
+            }
+        }
+        if number != 0 {
+            numbers.push(number);
         }
 
-        let candidate_range = input[i];
-        let highest_range_index = ranges.len() - 1;
-        let highest_range = &mut ranges[highest_range_index];
-        if candidate_range.0 >= highest_range.0 && candidate_range.0 <= highest_range.1 {
-            // We have an overlap!
-            highest_range.1 = if candidate_range.1 > highest_range.1 {
-                candidate_range.1
-            } else {
-                highest_range.1
+        let op = iters[rows.len() - 1].next().unwrap();
+        if op != ' ' {
+            let total = match op {
+                '*' => numbers.into_iter().reduce(|acc, e| acc * e).unwrap(),
+                '+' => numbers.into_iter().reduce(|acc, e| acc + e).unwrap(),
+                _ => panic!("Unexpected operation!"),
             };
-        } else {
-            ranges.push(candidate_range);
+            totals.push(total);
+            numbers = vec![];
         }
     }
 
-    let mut distinct_ids = 0;
-    for (low, high) in ranges {
-        distinct_ids += high - low + 1;
-    }
-
-    println!("{}", distinct_ids);
+    println!("{:?}", totals.into_iter().reduce(|t, e| t + e).unwrap());
 }
